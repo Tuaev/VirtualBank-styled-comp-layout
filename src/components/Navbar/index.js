82569246b3782e43176564af4b059f30e637ec20
Soslan <sparksoslan@gmail.com>
2021-04-10 18:25:41 +0100

    Add main navbar and styled

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,10 +1,46 @@
 import React from 'react';
+import { FaBars } from 'react-icons/fa';
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLink,
+  NavBtn,
+  NavBtnLink,
+} from './NavbarStyles';
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>nabar</h1>
-    </div>
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">Virtual.</NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLink to="about">About</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="discover">Discover</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="services">Services</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="signip">Sign Up</NavLink>
+            </NavItem>
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLink to="/signin">Sign In</NavBtnLink>
+          </NavBtn>
+        </NavbarContainer>
+      </Nav>
+    </>
   );
 };
 
